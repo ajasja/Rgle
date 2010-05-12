668729649c1d2636d06d26a08195668d9fe4d06a
Ajasja Ljubetic <ajasja.ljubetic@gmail.com>
2010-05-12 19:43:19 +0200

    some minor corrections to example parabola.rb

--- lib/examples/parabola.rb
+++ lib/examples/parabola.rb
@@ -9,11 +9,11 @@ gle = RGleBuilder.build do
   beg :graph do
     title "Parabola"
     xtitle "x"
-    ytitle '"f(x) = (x-2)^2"'
+    ytitle '"f(x) = (x-2)^2"' #the string quoting is cumbersome...
     let "d1 = (x-2)^2 from 0 to 4 step 0.1"
 
     key "pos br compact"
-    d1 "key \"parabola\""
+    d1 "key \"parabola\"" 
     d1 :line, :color, :red
   end
 end
