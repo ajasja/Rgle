3d546b68359d8f8082da6f3d745ee52ecf7ea898
Ajasja Ljubetic <ajasja.ljubetic@gmail.com>
2010-05-21 18:58:52 +0200

    just playing around:)

--- lib/examples/2_sql/sql_rgle.rb
+++ lib/examples/2_sql/sql_rgle.rb
@@ -1,4 +1,5 @@
-require File.dirname(__FILE__)+'/../../rgle'
+#require File.dirname(__FILE__)+'/../../rgle'
+require 'rgle'
 include RGle
 
 def get_number_of_columns(afile, header=true, comment_char='!', separator=',', nan_string='NaN')
@@ -29,7 +30,7 @@ gle = RGleBuilder.build "sql_rgle" do
   thumbsize 10, 10
   database 'test_db'
   
-  resid=6
+  resid=16
   #execute query immediately, so that one can read the number of columns that have a non NaN value
   sql! "Select frame, phi, psi, chi1,chi2,chi3,chi4,chi5  from dihedrals where resid=#{resid}"
   numcols = get_number_of_columns "sql_rgle.csv"
@@ -67,7 +68,7 @@ gle = RGleBuilder.build "sql_rgle" do
 
       data "sql_rgle.csv d1=c1,c#{i+3} d2=c1,c#{i+3}"
 
-      d1 "color red marker dot mscale 0.8"
+      d1 "color red marker dot mscale 0.9"
       d2 "deresolve 5 average line color red"
     end
   }
